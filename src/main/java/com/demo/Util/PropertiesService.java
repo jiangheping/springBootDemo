d54d6112a8f2b3fbeba93686b6f17466048d9063
jiangheping1 <jiangheping01@chinatopcredit.com>
2018-07-26 22:27:05 +0800

    @Value的使用（springboot默认启动时就加载application.properties）

---
+++ src/main/java/com/demo/Util/PropertiesService.java
@@ -0,0 +1,22 @@
+package com.demo.Util;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * application.properties在spring boot启动时默认加载此文件
+ * 自定义属性文件通过@PropertySource加载
+ */
+
+@Configuration
+@Data
+public class PropertiesService {
+
+    @Value("${user.name.value}")
+    private String userName;
+
+    @Value("${user.age.value}")
+    private int userAge;
+}
--- src/main/java/com/demo/controller/HelloWorldController.java
+++ src/main/java/com/demo/controller/HelloWorldController.java
@@ -1,7 +1,9 @@
 package com.demo.controller;
 
+import com.demo.Util.PropertiesService;
 import com.demo.entity.User;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,16 +12,22 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class HelloWorldController {
 
+    @Autowired
+    private PropertiesService propertiesService;
+
     @RequestMapping("/hello")
     public String getHello() {
         return "Hello World";
     }
 
-    //返回json形式： {"name":"小明","age":28}
+    //返回json形式： {"name":"xx","age":xx}
     @RequestMapping("/getUser")
     public User getUser() {
         log.info(" HelloWorldController getUser ");
-        return User.builder().name("小明").age(28).build();
+        return User.builder()
+                .name(propertiesService.getUserName())
+                .age(propertiesService.getUserAge())
+                .build();
     }
 
 }
